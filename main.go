81dcd014a9ef6126fae3e09c7427d1b49eafebaa
Tim-Restart <tim_restart@proton.me>
2025-07-25 10:43:30 +1000

    Takes multiple IP address

--- main.go
+++ main.go
@@ -12,12 +12,13 @@ import (
 func main() {
 
 
-
-	switch len(os.Args) {
-	case 1:
+	
+	queries := len(os.Args)
+	switch {
+	case queries == 1:
 		fmt.Println("no IP address provided")
 		os.Exit(1)
-	case 2:
+	case queries == 2:
 		ip := os.Args[1]
 		result, err := getIP(ip)
 		if err != nil {
@@ -25,8 +26,26 @@ func main() {
 			fmt.Println(err)
 			return
 		}
+		fmt.Println(result.Zip)
 		fmt.Println(result.City)
-		
+		fmt.Println(result.Region)
+	case queries > 2:
+		i := 1
+		for i < len(os.Args) {
+			ip := os.Args[i]
+			result, err := getIP(ip)
+			if err != nil {
+				fmt.Println("Error sending request")
+				fmt.Println(err)
+				return
+			}
+			fmt.Println(result.Zip)
+			fmt.Println(result.City)
+			fmt.Println(result.Region)
+			i++
 		}
+	default:
+		fmt.Println("Incorrect queries, please try again")
+	}
 	}
 
